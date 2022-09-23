a86201e5d8e3f105da0afbcb0daf8eced1b0ed41
sohamdev2 <soham@rivuletdigital.com>
2022-09-23 11:43:52 +0530

    build script updated

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,33 +1,54 @@
+// import { h, defineComponent, nextTick } from "vue";
+import { h } from "vue";
 /**
  * Component props Object
  */
 const props = {
-    text: {
-        type: String,
-        default: 'hello'
-    },
-}
+  text: {
+    type: String,
+    default: "hello",
+  },
+};
 
 /**
  * Component Hello
  */
 const Hello = {
-  name: 'hello',
-//   mixins: [ BaseMixin ],
-  props,
-  data () {
-    return {
-      message: this.text
+  name: "hello",
+  //   mixins: [ BaseMixin ],
+  render() {
+    try {
+      this.error = false;
+      //   const { $slots, $attrs, tag, componentData, realList, getKey } = this;
+      //   const componentStructure = computeComponentStructure({
+      //     $slots,
+      //     tag,
+      //     realList,
+      //     getKey,
+      //   });
+      //   this.componentStructure = componentStructure;
+      //   const attributes = getComponentAttributes({ $attrs, componentData });
+      //   return componentStructure.render(h, attributes);
+      return h("div", "hello");
+    } catch (err) {
+      //   this.error = true;
+      return h("pre", { style: { color: "red" } }, err.stack);
     }
+  },
+  props,
+  data() {
+    return {
+      message: this.text,
+    };
   },
   methods: {
     /**
      * Main loop. Draws visualization.
      */
     mainLoop: function () {
-      console.log('loop')
+      console.log("loop");
     },
-  }
-}
+  },
+};
 
-export default Hello
+export default Hello;
